cbc50e4600dbdb79a108be9655175a772f967d1e
joris <joris.tillet@ensta-bretagne.org>
2016-11-07 10:04:04 +0100

    Ajouts des fichiers de reconnaissance et de tracking, et màj de init_py_v2.bash

--- py/init_py_v2.bash
+++ py/init_py_v2.bash
@@ -1,2 +1,2 @@
-export PYTHONPATH=`pwd`/../pynaoqi-python2.7-2.1.4.13-linux64/
-export LD_LIBRARY_PATH=`pwd`/../pynaoqi-python2.7-2.1.4.13-linux64/
+export PYTHONPATH="/home/joris/Documents/Travail Github/NaoSafeWalk/pynaoqi-python2.7-2.1.4.13-linux64/"
+export LD_LIBRARY_PATH="/home/joris/Documents/Travail Github/NaoSafeWalk/pynaoqi-python2.7-2.1.4.13-linux64/"
